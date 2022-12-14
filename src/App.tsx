d652e17feeafd1cba3e4684475a31af0ac8f13d8
David Shing <dvdshing@gmail.com>
2022-12-14 23:09:33 +0100

    Added methods to retrieve data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data/dishes.json'
+
+export interface Root {
+  dishes: Dish[]
+}
+
+export interface Dish {
+  id: number
+  name: string
+  restaurant: string
+  availableMeals: string[]
+}
+
+// get unique types
+export const unique = [...new Set(data.dishes.map(item => item.availableMeals).flat())];
+const unique_pair = unique.map( item => ({
+  [item]: item.charAt(0).toUpperCase() + item.slice(1)
+}));
+
+// get restaurants based on type
+export const results = data.dishes.filter(item => item.availableMeals.includes("lunch"));
+
+function retrieveData (){
+  
+}
 
 function App() {
+  // for (var item in data.dishes){
+  //   console.log("Dish " + data.dishes[item].name)
+  // }
+  
+  // const items = results.map( item => {
+  //   console.log("Restaurants " + item.restaurant)
+  // });
+    console.log(unique_pair);
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/components/Forms.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+// import Form1 from "./Form1";
+// import Form2 from "./Form2";
+// import Form3 from "./Form3";
+// import FormReview from "./FormReview";
+// Parent component holding states.
+
+export interface FormProps{
+    formData: {
+        
+    }
+}
+
+function Forms(){
+    // const [page, setPage] = useState(0)
+    // const [formData, setFormData] = useState({
+    //     type: "",
+    // })
+    // const FormTitles = ["Type", "Restaurant", "Dish", "Review"]
+    // const formDisplay(){
+    //     if(page === 0){
+    //         return <Form1 formData={formData} setFormData={setFormData}/>
+    //     }else if (page === 1) {
+    //         return <Form2 formData={formData} setFormData={setFormData}/>
+    //     }else if (page === 2 ){
+    //         return <Form3 formData={formData} setFormData={setFormData}/>
+    //     }else {
+    //         return <FormReview formData={formData} setFormData={setFormData}/>
+    //     }
+    // }
+    // return <div>Forms</div>
+}
+
+export default Forms
